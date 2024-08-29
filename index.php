eb6f31a119317e35eb72c5c2451e376dec7ee7d6
markmatakili2 <markmatakili84@gmail.com>
2024-08-29 15:26:59 +0300

    updates

--- index.php
+++ index.php
@@ -1,61 +1,63 @@
 <?php
-require 'db.php';
+include 'db.php';
 ?>
-<!DOCTYPE>
-    <html>
-    <head>
-        <meta charset = "UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-        <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
-        <title>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
+    <title>
         shopping
-        </title>
-        <link rel="stylesheet" href="style2.css">
-    </head>
+    </title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
 <body>
-<header>
-    <div>
-    <h1>shopping</h1>
-</div>
+    <header>
+        <div>
+            <h1>shopping</h1>
+        </div>
 
-<div>
-<nav>
-    <ul> 
-        
-        <li>
-            <a href="">
-        <i class='bx bx-cart'></i>
-        <p>cart</p>
-    </a>
-        </li>
-    
-    
-        <li>
-             <a href="">
-        <i class='bx bx-cog' ></i>
-        <p>setting</p>
-    </a>
-        </li>
+        <div>
+            <nav>
+                <ul>
+
+                    <li>
+                        <a href="">
+                            <i class='bx bx-cart'></i>
+                            <p>cart</p>
+                        </a>
+                    </li>
 
 
-        <li>
-            <a href="">
-        <i class='bx bx-log-out' ></i>
-        <p>logout</p>
-    </a>
-        </li>
+                    <li>
+                        <a href="">
+                            <i class='bx bx-cog'></i>
+                            <p>setting</p>
+                        </a>
+                    </li>
 
-    </ul>
 
-</nav>
-</div>
+                    <li>
+                        <a href="">
+                            <i class='bx bx-log-out'></i>
+                            <p>logout</p>
+                        </a>
+                    </li>
 
-</header>
+                </ul>
 
-<main>
+            </nav>
+        </div>
 
-<section class = "hero">
-<div class="hero">
+    </header>
+
+    <main>
+
+        <section class="hero">
+            <div class="hero">
                 <div class="slides active" style="background-image: url(images/car1.jpeg);"></div>
                 <div class="slide" style="background-image: url(images/car2.jpeg);"></div>
                 <div class="slide" style="background-image: url(images/car3.jpeg);"></div>
@@ -64,56 +66,56 @@ require 'db.php';
                 <button id="prev">Previous</button>
                 <button id="next">Next</button>
             </div>
- 
-</section>
-<section class = "products">
-    <h1>top products</h1>
-    <div class="cards">
-     <div class="product1">
-        <div class="image">
-            <img src="car1.jpeg">
-        </div>
-         <div class="text">
-            <h1>car</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-         </div>
-     </div> 
-     <div class="product1">
-        <div class="image">
-            <img src="car1.jpeg">
-        </div>
-         <div class="text">
-            <h1>car</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-         </div>
-     </div>
-     <div class="product1">
-        <div class="image">
-            <img src="car1.jpeg">
-        </div>
-         <div class="text">
-            <h1>car</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-         </div>
-     </div> 
-     <div class="product1">
-        <div class="image">
-            <img src="car1.jpeg">
-        </div>
-         <div class="text">
-            <h1>car</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-         </div>
-     </div>
-     
 
-    </div>
-</section>
-</main>
+        </section>
+        <section class="products">
+            <h1>top products</h1>
+            <div class="cards">
+                <div class="product1">
+                    <div class="image">
+                        <img src="car1.jpeg">
+                    </div>
+                    <div class="text">
+                        <h1>car</h1>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="product1">
+                    <div class="image">
+                        <img src="car1.jpeg">
+                    </div>
+                    <div class="text">
+                        <h1>car</h1>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="product1">
+                    <div class="image">
+                        <img src="car1.jpeg">
+                    </div>
+                    <div class="text">
+                        <h1>car</h1>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="product1">
+                    <div class="image">
+                        <img src="car1.jpeg">
+                    </div>
+                    <div class="text">
+                        <h1>car</h1>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
 
-<footer>
 
-</footer>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
 
 
 
