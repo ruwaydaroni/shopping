454a2e437fabe4b28829df672209c5dfbc61202d
markmatakili2 <markmatakili84@gmail.com>
2024-09-19 17:25:47 +0300

    hjv

--- admin.php
+++ admin.php
@@ -2,47 +2,38 @@
 require 'db.php';
 ?>
 
-<DOCTYPE>
-    <html>
-        <head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
     <link rel="stylesheet" href="style2.css">
-
-    
+    <title>Admin Page</title>
 </head>
-<body>
-<?php
-require 'header.php';
-?>
-    <div class="sidebar">
-    <ul>
-        <li>
-          <a href="#">
-            dashboard
-        
-</a>
-        </li>
-        <li>
-          <a href="#">
-            add product
-        
-</a>
-        </li>
-        <li>
-          <a href="#">
-            delete products        
-</a>
-        </li>
-        <li>
-          <a href="#">
-            ordered items
-        
-</a>
-        </li>
-    </ul>
-</div>
 
+<body>
+    <?php
+    require 'header.php';
+    ?>
+
+    <div class="sidebar">
+        <ul>
+            <li>
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <a href="#">Add Product</a>
+            </li>
+            <li>
+                <a href="#">Delete Products</a>
+            </li>
+            <li>
+                <a href="#">Ordered Items</a>
+            </li>
+        </ul>
+    </div>
 </body>
 
+</html>
