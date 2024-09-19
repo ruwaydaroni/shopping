9cb64fe78dce06477171a84d6a7ba0a6dfb16f36
ruwaydaroni <ruwaydaronix2@gmail.com>
2024-09-19 17:19:10 +0300

    k

--- admin.php
+++ admin.php
@@ -9,68 +9,40 @@ require 'db.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
     <link rel="stylesheet" href="style2.css">
+
     
 </head>
 <body>
 <?php
 require 'header.php';
 ?>
-<div class="signup">
-        <div class="card">
-            <form action="admin.php" enctype="multipart/form-data" method="post">
-                <label for="pName">Product Name</label>
-                <input type="text" name="pName" placeholder="insert product name">
-                <label for="price">Price</label>
-                <input type="number" name="price" placeholder="insert price">
-                <label for="image">Upload Image</label>
-                <input type="file" name="image" placeholder="insert image">
-                <br>
-                <button type="submit" value="submit">submit</button>
-            </form>
-        </div>
+    <div class="sidebar">
+    <ul>
+        <li>
+          <a href="#">
+            dashboard
+        
+</a>
+        </li>
+        <li>
+          <a href="#">
+            add product
+        
+</a>
+        </li>
+        <li>
+          <a href="#">
+            delete products        
+</a>
+        </li>
+        <li>
+          <a href="#">
+            ordered items
+        
+</a>
+        </li>
+    </ul>
+</div>
+
 </body>
 
-<?php
-include 'db.php'; 
-if ($con) {
-} else {
-    die("Failed to establish MySQLi connection: " . $con->connect_error);
-}
-
-$pName = $_POST['pName']; 
-$price = $_POST['price']; 
-$image = $_FILES['image'];
-
-$targetDir = "uploads/";
-if (!file_exists($targetDir)) {
-    mkdir($targetDir, 0777, true);
-}
-
-$targetFile = $targetDir . basename($image["name"]);
-
-
-if (move_uploaded_file($image["tmp_name"], $targetFile)) {
-   
-    $sql = "INSERT INTO products (product_name, price, image) VALUES (?, ?, ?)";
-    $stmt = $con->prepare($sql);
-
-    if (!$stmt) {
-        die("Failed to prepare SQL statement: " . $con->error);
-    }
-
-
-    $stmt->bind_param("sds", $pName, $price, $image['name']);
-    $result = $stmt->execute();
-
-    if ($result) {
-        echo "Product submission successful!";
-    } else {
-        echo "Error: Could not submit product.";
-    }
-} else {
-    echo "Error: Image upload failed.";
-}
-
-
-$con->close();
-?>
